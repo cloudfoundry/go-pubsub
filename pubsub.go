be9d5caf91878be7edbc1d1ead81ee0768a9b1fb
Andrew Poydence <apoydence@gmail.com>
2017-07-16 06:46:31 -0600

    creates pubsub without concurrency support
    Creates a PubSub that can handle multiple subscribers defined by a TreeBuilder. Each data point is written to a subscriber described by a TreeTraverser

---
+++ pubsub.go
@@ -0,0 +1,108 @@
+package pubsub
+
+import (
+	"unsafe"
+)
+
+type PubSub struct {
+	b TreeBuilder
+	t TreeTraverser
+	n *node
+}
+
+type Data unsafe.Pointer
+
+type TreeBuilder interface {
+	PlaceSubscription(sub Subscription, location []string) (key string, ok bool)
+}
+
+type TreeTraverser interface {
+	Traverse(data Data, location []string) (keys []string)
+}
+
+func New(b TreeBuilder, t TreeTraverser) *PubSub {
+	return &PubSub{
+		b: b,
+		t: t,
+		n: newNode(),
+	}
+}
+
+type Subscription interface {
+	Write(data Data)
+}
+
+func (s *PubSub) Subscribe(subscription Subscription) {
+	s.traverseSubscribe(subscription, s.n, nil)
+}
+
+func (s *PubSub) Publish(d Data) {
+	s.traversePublish(d, s.n, nil)
+}
+
+type node struct {
+	children      map[string]*node
+	subscriptions []Subscription
+}
+
+func newNode() *node {
+	return &node{
+		children: make(map[string]*node),
+	}
+}
+
+func (n *node) addChild(key string) *node {
+	if n == nil {
+		return nil
+	}
+
+	if child, ok := n.children[key]; ok {
+		return child
+	}
+
+	child := newNode()
+	n.children[key] = child
+	return child
+}
+
+func (n *node) fetchChild(key string) *node {
+	if n == nil {
+		return nil
+	}
+
+	if child, ok := n.children[key]; ok {
+		return child
+	}
+
+	return nil
+}
+
+func (n *node) listSubscriptions() []Subscription {
+	if n == nil {
+		return nil
+	}
+
+	return n.subscriptions
+}
+
+func (s *PubSub) traversePublish(d Data, n *node, l []string) {
+	for _, sub := range n.listSubscriptions() {
+		sub.Write(d)
+	}
+
+	children := s.t.Traverse(d, l)
+
+	for _, child := range children {
+		s.traversePublish(d, n.fetchChild(child), append(l, child))
+	}
+}
+
+func (s *PubSub) traverseSubscribe(ss Subscription, n *node, l []string) {
+	child, ok := s.b.PlaceSubscription(ss, l)
+	if !ok {
+		n.subscriptions = append(n.subscriptions, ss)
+		return
+	}
+
+	s.traverseSubscribe(ss, n.addChild(child), append(l, child))
+}
---
+++ pubsub_test.go
@@ -0,0 +1,194 @@
+package pubsub_test
+
+import (
+	"log"
+	"strings"
+	"testing"
+	"unsafe"
+
+	"github.com/apoydence/onpar"
+	. "github.com/apoydence/onpar/expect"
+	. "github.com/apoydence/onpar/matchers"
+	"github.com/apoydence/pubsub"
+)
+
+type TPS struct {
+	*testing.T
+	p             *pubsub.PubSub
+	treeBuilder   *spyTreeBuilder
+	treeTraverser *spyTreeTraverser
+	subscription  *spySubscription
+}
+
+func TestPubSub(t *testing.T) {
+	o := onpar.New()
+	defer o.Run(t)
+	o.BeforeEach(func(t *testing.T) TPS {
+		spyB := newSpyTreeBuilder()
+		spyT := newSpyTreeTraverser()
+
+		return TPS{
+			T:             t,
+			subscription:  newSpySubscrption(),
+			p:             pubsub.New(spyB, spyT),
+			treeBuilder:   spyB,
+			treeTraverser: spyT,
+		}
+	})
+
+	o.Spec("it invokes the TreeBuilder for each level of a subscription", func(t TPS) {
+		t.treeBuilder.keys = map[string]string{
+			"":    "a",
+			"a":   "b",
+			"a-b": "",
+		}
+		t.p.Subscribe(t.subscription)
+
+		Expect(t, t.treeBuilder.locations).To(HaveLen(3))
+
+		for k := range t.treeBuilder.keys {
+			Expect(t, t.treeBuilder.locations).To(Contain(k))
+		}
+		for _, s := range t.treeBuilder.subs {
+			Expect(t, s).To(Equal(t.subscription))
+		}
+	})
+
+	o.Spec("it invokes the TreeTraverser for each level", func(t TPS) {
+		t.treeTraverser.keys = map[string][]string{
+			"":      []string{"a", "b"},
+			"a":     []string{"a", "b"},
+			"b":     []string{"a", "b"},
+			"a-a":   []string{"a", "b"},
+			"a-b":   nil,
+			"b-a":   nil,
+			"b-b":   nil,
+			"a-a-a": nil,
+			"a-a-b": nil,
+		}
+
+		t.p.Publish(Data("x"))
+
+		Expect(t, t.treeTraverser.locations).To(HaveLen(9))
+		for k := range t.treeTraverser.keys {
+			Expect(t, t.treeTraverser.locations).To(Contain(k))
+		}
+	})
+
+	o.Spec("it writes to the correct subscription", func(t TPS) {
+		sub1 := newSpySubscrption()
+		sub2 := newSpySubscrption()
+		sub3 := newSpySubscrption()
+		t.treeBuilder.keys = map[string]string{
+			"":      "a",
+			"a":     "b",
+			"a-b":   "c",
+			"a-b-c": "",
+		}
+		t.p.Subscribe(sub1)
+
+		t.treeBuilder.keys = map[string]string{
+			"":      "a",
+			"a":     "b",
+			"a-b":   "d",
+			"a-b-d": "",
+		}
+		t.p.Subscribe(sub2)
+
+		t.treeBuilder.keys = map[string]string{
+			"":  "a",
+			"a": "",
+		}
+		t.p.Subscribe(sub3)
+
+		t.treeTraverser.keys = map[string][]string{
+			"":      []string{"a", "x"},
+			"x":     nil,
+			"a":     []string{"b", "y"},
+			"a-y":   nil,
+			"a-b":   []string{"c", "z"},
+			"a-b-z": nil,
+			"a-b-c": nil,
+		}
+		t.p.Publish(Data("some-data"))
+
+		Expect(t, sub1.data).To(HaveLen(1))
+		Expect(t, sub2.data).To(HaveLen(0))
+		Expect(t, sub3.data).To(HaveLen(1))
+
+		Expect(t, readData(sub1.data[0])).To(Equal("some-data"))
+		Expect(t, readData(sub3.data[0])).To(Equal("some-data"))
+	})
+}
+
+func Data(s string) pubsub.Data {
+	b := []byte(s)
+	return pubsub.Data(unsafe.Pointer(&b))
+}
+
+func readData(d pubsub.Data) string {
+	return string(*(*[]byte)(unsafe.Pointer(d)))
+}
+
+type spyTreeTraverser struct {
+	keys      map[string][]string
+	locations []string
+	data      []pubsub.Data
+}
+
+func newSpyTreeTraverser() *spyTreeTraverser {
+	return &spyTreeTraverser{}
+}
+
+func (s *spyTreeTraverser) Traverse(data pubsub.Data, location []string) []string {
+	s.data = append(s.data, data)
+
+	key := strings.Join(location, "-")
+	s.locations = append(s.locations, key)
+	result, ok := s.keys[key]
+	if !ok {
+		log.Panicf("unknown location: %s", key)
+	}
+
+	return result
+}
+
+type spyTreeBuilder struct {
+	keys      map[string]string
+	subs      []pubsub.Subscription
+	locations []string
+}
+
+func newSpyTreeBuilder() *spyTreeBuilder {
+	return &spyTreeBuilder{}
+}
+
+func (s *spyTreeBuilder) PlaceSubscription(sub pubsub.Subscription, location []string) (string, bool) {
+	s.subs = append(s.subs, sub)
+
+	key := strings.Join(location, "-")
+	s.locations = append(s.locations, key)
+	result, ok := s.keys[key]
+
+	if !ok {
+		log.Panicf("unknown location: %s", key)
+	}
+
+	if result == "" {
+		return "", false
+	}
+
+	return result, true
+}
+
+type spySubscription struct {
+	data []pubsub.Data
+}
+
+func newSpySubscrption() *spySubscription {
+	return &spySubscription{}
+}
+
+func (s *spySubscription) Write(data pubsub.Data) {
+	s.data = append(s.data, data)
+}
